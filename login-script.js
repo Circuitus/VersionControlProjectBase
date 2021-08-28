6ebf5acbef2f9c9e9c6e00d945ad30adceda21ae
Boussole <54520152+Circuitus@users.noreply.github.com>
2021-08-28 01:56:07 +0000

    Added firebase at a basic level for login purposes

---
+++ login-script.js
@@ -0,0 +1,12 @@
+// Fires on Submission of the Login Page
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-$(document).ready(function(){
-    $('.header').height($(window).height());
-})
+// firebase.initializeApp({
+//   apiKey: "AIzaSyBLyoJRTTv3EaEZPBMIs__ZK4osNCAUGL8",
+//   authDomain: "projectbase-67ef9.firebaseapp.com",
+//   projectId: "projectbase-67ef9"
+// });
 
-var ready = (callback) => {
-    if (document.readyState != "loading") callback();
-    else document.addEventListener("DOMContentLoaded", callback);
-}
-ready(() => {
-    document.querySelector(".header").style.height = window.innerHeight + "px";
-})
+// const db = firebase.firestore();
+
+// $(document).ready(function(){
+//     $('.header').height($(window).height());
+// })
+
+// var ready = (callback) => {
+//     if (document.readyState != "loading") callback();
+//     else document.addEventListener("DOMContentLoaded", callback);
+// }
+
+// ready(() => {
+//     document.querySelector(".header").style.height = window.innerHeight + "px";
+// });
+
