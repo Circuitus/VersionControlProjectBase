db83e735f6c48bea6e87c836da0b278039987468
Boussole <54520152+Circuitus@users.noreply.github.com>
2021-08-28 02:45:12 +0000

    Added log out button

--- main.js
+++ main.js
@@ -6,6 +6,31 @@ firebase.initializeApp({
 
 const db = firebase.firestore();
 
+// firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+//   .then(() => {
+//     // Existing and future Auth states are now persisted in the current
+//     // session only. Closing the window would clear any existing state even
+//     // if a user forgets to sign out.
+//     // ...
+//     // New sign-in will be persisted with session persistence.
+//     return firebase.auth().signInWithEmailAndPassword(email, password);
+//   })
+//   .catch((error) => {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//   });
+  
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log(user);
+  } else {
+    console.log(window.location);
+    // check window.location !== login / signup first.
+    // Redirect away from page.
+  }
+});
+
 $(document).ready(function(){
     $('.header').height($(window).height());
 })
@@ -19,6 +44,12 @@ ready(() => {
     document.querySelector(".header").style.height = window.innerHeight + "px";
 });
 
+/*
+  If the user is on mainpage & they arent logged in, or there session has expired -> Redirect
+
+  Input form validation, to ensure we dont send any invalid information to firebase, and alert the user accordingly.
+*/
+
 const signup = () => {
   const email = $("#signup-email").val();
   const username = $("#signup-username").val();
@@ -26,6 +57,10 @@ const signup = () => {
 
   firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((usr) => {
+      return usr.user.updateProfile({
+        displayName: username
+      })
+
       db.collection("users").add({
         username: username,
         email: email,
@@ -52,3 +87,11 @@ const login = () => {
       console.error(error);
     });
 };
+
+const logout = () => {
+  firebase.auth().signOut().then(() => {
+    window.location.href = "login.html";
+  }).catch((error) => {
+    console.error(error);
+  });
+}
