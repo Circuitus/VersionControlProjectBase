8f7d4af8f3d0b3fe4f4a35218ca2bd59368abc1b
Boussole <54520152+Circuitus@users.noreply.github.com>
2021-08-28 02:17:17 +0000

    User signup implemented

--- main.js
+++ main.js
@@ -1,21 +1,44 @@
-// firebase.initializeApp({
-//   apiKey: "AIzaSyBLyoJRTTv3EaEZPBMIs__ZK4osNCAUGL8",
-//   authDomain: "projectbase-67ef9.firebaseapp.com",
-//   projectId: "projectbase-67ef9"
-// });
+firebase.initializeApp({
+  apiKey: "AIzaSyBLyoJRTTv3EaEZPBMIs__ZK4osNCAUGL8",
+  authDomain: "projectbase-67ef9.firebaseapp.com",
+  projectId: "projectbase-67ef9"
+});
 
-// const db = firebase.firestore();
+const db = firebase.firestore();
 
-// $(document).ready(function(){
-//     $('.header').height($(window).height());
-// })
+$(document).ready(function(){
+    $('.header').height($(window).height());
+})
 
-// var ready = (callback) => {
-//     if (document.readyState != "loading") callback();
-//     else document.addEventListener("DOMContentLoaded", callback);
-// }
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
 
-// ready(() => {
-//     document.querySelector(".header").style.height = window.innerHeight + "px";
-// });
+ready(() => {
+    document.querySelector(".header").style.height = window.innerHeight + "px";
+});
 
+const signup = () => {
+  const email = $("#signup-email").val();
+  const username = $("#signup-username").val();
+  const password = $("#signup-password").val();
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((usr) => {
+      db.collection("users").add({
+        username: username,
+        email: email,
+        files: []
+      }).then(e => {
+        console.log(e);
+        window.location.href = "mainpage.html";
+      })
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+      console.error(error);
+    });
+};
